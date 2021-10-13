dcf98e3e3e3e5bbf94d2718b264205c1f5e13c88
VazkenA98 <vazken99@gmail.com>
2021-10-13 17:21:53 +0400

    test 2 branch Test class

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("555");
-        System.out.println("999");
+        System.out.println("777");
     }
 }
