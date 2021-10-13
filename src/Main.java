bfb559fffc198b8bfe2402d8fb2ae54f5801ce0e
VazkenA98 <vazken99@gmail.com>
2021-10-13 17:25:24 +0400

    test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("555");
+        System.out.println("nb_1");
+        System.out.println("nb_1");
     }
 }
